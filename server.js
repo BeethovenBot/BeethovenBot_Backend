50f030bcc584c1057bd72290c1b59b00b5635b2a
unknown <mozartcuidadordigital@gmail.com>
2025-06-21 15:08:29 -0500

    123

---
+++ server.js
@@ -0,0 +1,37 @@
+// backend/src/server.js
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./src/config/db');
+const saludMentalRoutes = require('./src/routes/saludMentalRoutes');
+const bodyParser = require('body-parser');
+const cors = require('cors'); // Importar cors
+const app = express();
+
+dotenv.config(); // Cargar variables de entorno
+connectDB(); // Conectar a la base de datos
+
+
+// Aumentar el límite del tamaño del cuerpo
+app.use(bodyParser.json({ limit: '10mb' })); // Ajusta el límite según sea necesario
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
+
+// Middleware para permitir CORS desde cualquier origen (puedes restringirlo a tu frontend en producción)
+app.use(cors());
+
+// Middleware para parsear el cuerpo de las peticiones como JSON
+app.use(express.json());
+
+// Montar las rutas de llamadas
+app.use('/api/saludMental', saludMentalRoutes);
+
+// Ruta básica de prueba
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  console.log(`Environment: ${process.env.NODE_ENV}`);
+});
---
+++ src/routes/saludMentalRoutes.js
@@ -0,0 +1,52 @@
+// backend/src/routes/callRoutes.js
+const express = require('express');
+const router = express.Router();
+const {onBoardingSaludMental, responseOnBoardingSaludMental, consultOnBoardingSaludMental} = require('../controllers/saludMentalController');
+const {entrevistaSaludMental, responseEntrevistaSaludMental, consultEntrevistaSaludMental} = require('../controllers/saludMentalController');
+const {seguimientoSaludMental, responseSeguimientoSaludMental, consultSeguimientoSaludMental} = require('../controllers/saludMentalController');
+const {whatsappBienvenida, whatsappMedicamento, whatsappCita} = require('../controllers/saludMentalController');
+const { chatConAsistenteHoteles } = require('../controllers/saludMentalController');
+const { responseOxiAsistencia, oxiOnBoarding, responseOxiOnBoarding, consultOxiOnBoarding } = require('../controllers/saludMentalController');
+const { oxiTamizaje, responseOxiTamizaje, consultOxiTamizaje } = require('../controllers/saludMentalController');
+
+// Ruta para onBoarding Salud Mental
+router.post('/onBoardingSaludMental', onBoardingSaludMental);
+router.post('/responseOnBoardingSaludMental', responseOnBoardingSaludMental);
+router.get('/consultOnBoardingSaludMental', consultOnBoardingSaludMental);
+
+// Ruta para Entrevista Salud Mental
+router.post('/entrevistaSaludMental', entrevistaSaludMental);
+router.post('/responseEntrevistaSaludMental', responseEntrevistaSaludMental);
+router.get('/consultEntrevistaSaludMental', consultEntrevistaSaludMental);
+
+// Ruta para Seguimiento Salud Mental
+router.post('/seguimientoSaludMental', seguimientoSaludMental);
+router.post('/responseseguimientoSaludMental', responseSeguimientoSaludMental);
+router.get('/consultSeguimientoSaludMental', consultSeguimientoSaludMental);
+
+// Rutas para WhatsApp
+router.post('/whatsappBienvenida', whatsappBienvenida);
+router.post('/whatsappMedicamento', whatsappMedicamento);
+router.post('/whatsappCita', whatsappCita);
+
+// Ruta para chat con el asistente de hoteles
+router.post('/chatHoteles', chatConAsistenteHoteles);
+
+//Ruta pra Oxi Presentación inicial
+//router.post('/oxiPresentacion', oxiPresentacion);
+
+// Ruta Oxi Asistencia
+router.post('/responseOxiAsistencia',responseOxiAsistencia );
+
+// Rutas para Oxi OnBoarding
+router.post('/oxiOnBoarding', oxiOnBoarding);
+router.post('/responseOxiOnBoarding', responseOxiOnBoarding);
+router.get('/consultOxiOnBoarding', consultOxiOnBoarding);
+
+// Rutas para Oxi Tamizaje
+router.post('/oxiTamizaje', oxiTamizaje);
+router.post('/responseOxiTamizaje', responseOxiTamizaje);
+router.get('/consultOxiTamizaje', consultOxiTamizaje);
+
+
+module.exports = router;
