ae6eddbc8367e74c6c135e1a79f3123a5ab4709e
Mundano-Creator <Mundano-Creator@users.noreply.github.com>
2025-07-10 18:13:27 -0500

    Historial

--- src/routes/BeethovenRoutes.js
+++ src/routes/BeethovenRoutes.js
@@ -6,8 +6,10 @@ const upload = multer();
 
 const { consultaMano } = require('../controllers/BeethovenController');
 const { procesarOCR } = require('../controllers/OcrController');
+const { guardarHistorial } = require('../controllers/historialController');
 
 router.post('/consulta', consultaMano);
 router.post('/ocr', upload.array('imagenes'), procesarOCR);
+router.post('/historial', guardarHistorial);
 
 module.exports = router;
